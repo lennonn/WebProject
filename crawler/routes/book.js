15ae4a2873d9b682226cdc0b7cf7cbe88f4a9b01
bjqlkj73 <bjqlkj73@ql88.com>
2018-07-27 14:37:29 +0800

    crawler

---
+++ crawler/routes/book.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+const book = require("../spider/book/book");
+
+router.get('/book', function(req, res, next) {
+    book.main("https://www.zwdu.com/book/26480/");
+    res.send('book crawer is spider');
+});
+
+module.exports = router;
